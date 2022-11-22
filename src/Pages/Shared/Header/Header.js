05604f14dab8ba8dcdf578a191b76b727bbbe3a8
sabbir hossain <mdsabbir477470@gmail.com>
2022-11-22 21:49:17 +0600

    some added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -11,11 +11,11 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#features"><h5 className='text-secondary'>Home</h5></Nav.Link>
-            <Nav.Link href="#features"><h5 className='text-secondary'>Home</h5></Nav.Link>
-            <Nav.Link href="#features"><h5 className='text-secondary'>Home</h5></Nav.Link>
-            <Nav.Link href="#features"><h5 className='text-secondary'>Home</h5></Nav.Link>
-            <Nav.Link href="#features"><h5 className='text-secondary'>Home</h5></Nav.Link>
+            <Nav.Link to='/home'><h5 className='text-secondary'>Home</h5></Nav.Link>
+            <Nav.Link to='/about'><h5 className='text-secondary'>About</h5></Nav.Link>
+            <Nav.Link to='/projects'><h5 className='text-secondary'>Projects</h5></Nav.Link>
+            <Nav.Link to='/blogs'><h5 className='text-secondary'>Blogs</h5></Nav.Link>
+            <Nav.Link to='/contact'><h5 className='text-secondary'>Contact</h5></Nav.Link>
           </Nav>  
         </Navbar.Collapse>
       </Container>
