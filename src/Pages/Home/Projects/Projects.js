485efe66f447d6a065f63963beda7bdb4c9dd2c4
sabbir hossain <mdsabbir477470@gmail.com>
2022-11-30 19:46:30 +0600

    dev bir same edit

--- src/Pages/Home/Projects/Projects.js
+++ src/Pages/Home/Projects/Projects.js
@@ -1,16 +1,58 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Card, CardGroup, Col, Container, Row } from 'react-bootstrap';
 
 const Projects = () => {
     return (
-            <div>
-            <h1 className='text-center mt-5 my-5'>My <span className='text-primary'>Projects</span></h1>
-            <Container>
-                <Row>
-                    <Col md="12">Projects</Col>
-                </Row>
-            </Container>
-        </div>
+        <Container className='mt-5 my-5'>
+            <h1 className='text-center'>My <span className='text-primary'>Projects</span></h1>
+            <Row>
+                <Col md="12">
+                    <CardGroup>
+                        <Card className='mx-3 my-5'>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in
+                                to additional content. This content is a little bit longer.
+                            </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card className='mx-3 my-5'>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This card has supporting text below as a natural lead-in to
+                                additional content.{' '}
+                            </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
+                        <Card className='mx-3 my-5'>
+                            <Card.Img variant="top" src="holder.js/100px160" />
+                            <Card.Body>
+                            <Card.Title>Card title</Card.Title>
+                            <Card.Text>
+                                This is a wider card with supporting text below as a natural lead-in
+                                to additional content. This card has even longer content than the
+                                first to show that equal height action.
+                            </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                            <small className="text-muted">Last updated 3 mins ago</small>
+                            </Card.Footer>
+                        </Card>
+                    </CardGroup>
+                </Col>
+            </Row>
+        </Container>
+
     );
 };
 
