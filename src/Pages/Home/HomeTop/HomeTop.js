5c300faecd061b14d5f6c6d26c248aa4fcfc853a
sabbir hossain <mdsabbir477470@gmail.com>
2022-12-09 15:35:43 +0600

    done

--- src/Pages/Home/HomeTop/HomeTop.js
+++ src/Pages/Home/HomeTop/HomeTop.js
@@ -16,7 +16,11 @@ const HomeTop = () => {
       
     return (
         <div className='home-Banner p-1' id='home'>
-
+            <div className="position-fixed fixed-bottom text-end me-4 mb-4">
+                  <a href="#home">
+                      <img className='img-fluid go-to-top-icon' src='https://icon-library.com/images/back-to-top-icon-png/back-to-top-icon-png-7.jpg' alt="back-to-top-icon" />
+                  </a>
+              </div>
             <div className='position-relative w-100 h-100 home-top'>
                 {/* <HomeBanner /> */}
             </div>
